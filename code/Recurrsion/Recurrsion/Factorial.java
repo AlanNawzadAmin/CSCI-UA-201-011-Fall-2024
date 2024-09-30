36781b9a63eba898abcb0c8db6b287cac0569f9e
Alan N Amin <alannamin@10-19-167-194.dynapool.wireless.nyu.edu>
2024-09-30 14:07:09 -0400

    code

---
+++ code/Recurrsion/Recurrsion/Factorial.java
@@ -0,0 +1,10 @@
+package Recurrsion;
+
+public class Factorial {
+	public static int factorial(int n){
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Factorial.factorial(10));
+	}
+}
---
+++ code/Recurrsion/Recurrsion/Fibonacci.java
@@ -0,0 +1,13 @@
+package Recurrsion;
+
+public class Fibonacci {
+	private static int first = 1;
+	private static int second = 1;
+	public static int fibonacci(int n) {
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Fibonacci.fibonacci(45));
+//		System.out.println(Fibonacci.fibonacci_fast(45)[1]);
+	}
+}
